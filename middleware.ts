7ef33b6d891d1f34c4fad892109c41a428e78d18
Jayant Saraswat <jayantsaraswat09@gmail.com>
2024-08-07 20:27:11 +0530

    initial comit

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// };
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Define routes that should be protected
+const isProtectedRoute = createRouteMatcher([
+  '/' // Add any additional routes here
+]);// Update clerkMiddleware to manually protect routes
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect(); // Protect the route if it matches the defined criteria
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).)", "/", "/(api|trpc)(.)"],
+};
