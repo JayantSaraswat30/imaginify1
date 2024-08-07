b638e2ea1da45b1cd941d1851187fd5a45bc2644
Jayant Saraswat <jayantsaraswat09@gmail.com>
2024-08-07 22:15:41 +0530

    second commit

--- middleware.ts
+++ middleware.ts
@@ -10,19 +10,36 @@
 //     '/(api|trpc)(.*)',
 //   ],
 // };
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// // Define routes that should be protected
+// const isProtectedRoute = createRouteMatcher([
+//   '/' // Add any additional routes here
+// ]);// Update clerkMiddleware to manually protect routes
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) {
+//     auth().protect(); // Protect the route if it matches the defined criteria
+//   }
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+.[w]+$|_next).)", "/", "/(api|trpc)(.)"],
+// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Define routes that should be protected
-const isProtectedRoute = createRouteMatcher([
-  '/' // Add any additional routes here
-]);// Update clerkMiddleware to manually protect routes
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect(); // Protect the route if it matches the defined criteria
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
   }
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).)", "/", "/(api|trpc)(.)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
